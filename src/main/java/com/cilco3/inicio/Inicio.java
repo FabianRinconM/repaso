ec58441f93b5bffef3adf89bd9717abb5b5f9ed0
FabianRinconM <exitofabian@gmail.com>
2022-08-13 09:29:56 -0500

    commit inicial del proyecto

--- src/main/java/com/cilco3/inicio/Inicio.java
+++ src/main/java/com/cilco3/inicio/Inicio.java
@@ -4,6 +4,7 @@ public class Inicio {
 
     public static void main(String[] args) {
         System.out.println("Hola desarrolladores");
+        System.out.println("Hola desarrolladores");
 
     }
 }
